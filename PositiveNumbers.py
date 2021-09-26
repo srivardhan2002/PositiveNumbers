9c760b9e8e1be2758069422e06dabe6d75a5e7d6
srivardhan2002 <73741897+srivardhan2002@users.noreply.github.com>
2021-09-26 20:49:04 +0530

    Create PositiveNumber.cpp
    Wrote a python program which takes 5 inputs and stores in list1 and takes 4 inputs and stores in list2 and prints only the positive values.

---
+++ PositiveNumbers.py
@@ -0,0 +1,17 @@
+list1 = [] 
+
+for i in range(5):
+  e = input()
+  list1.append(e)
+
+for i in list1:
+  print(i)
+  
+list2 = []
+
+for i in range(4):
+  e = input()
+  list2.append(e)
+
+for i in list2:
+  print(i)
